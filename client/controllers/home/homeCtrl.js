e9561368e55285c9b15b9bab94eeda0848d0faef
BerkayCavus <berkay.cavus92@gmail.com>
2016-01-21 16:43:23 +0200

    need delete/update post

--- client/controllers/home/homeCtrl.js
+++ client/controllers/home/homeCtrl.js
@@ -1,15 +1,20 @@
  var homeApp = angular.module('homeApp', [])    
  
     .controller('userController',function($scope,$http){
-        var apiData = [], userNames = [] , userPosts = [];                
-        $http({
-            method :'GET',
-            url : '/api/user'
-        })
-        .success(function(data,status,headers,config){
-            apiData = data
-            getValues(apiData);       
-        }) 
+        var apiData = [], userNames = [] , userPosts = [];
+        
+        
+        var refreshData = function(){
+            $http({
+                 method :'GET',
+                 url : '/api/user'
+            })
+             .success(function(data,status,headers,config){
+                 apiData = data
+                 getValues(apiData);       
+            }) 
+        }  
+        refreshData()            
         $scope.showUser = true;
         $scope.showPost = true;
         $scope.showUserForm = true;
@@ -41,6 +46,33 @@
                 console.log(data)
                 $scope.allNames.push(dataObject.username)
             })
+            $scope.username = ''
+            $scope.password = ''
+            $scope.email    = ''
+            refreshData()
+        }
+        $scope.openDialogModal = function(index){
+            var user = userNames[index]        
+            console.log(user)
+            $scope.selectedUser = user;
+            $scope.updateUser = function(){
+                var newUserName = $scope.newName
+                var newUserId = getUserId(user)
+                var updatedObject = {'username' : newUserName}              
+                $http({
+                    method : 'PUT',
+                    url    : '/api/user/' + newUserId,
+                    data   : updatedObject
+                })
+                .success(function(data,status,headers,config){
+                    console.log(data)
+                    console.log(user + ' is updated to ' + newUserName)
+                    removeData(index,userNames)
+                    updateData(index,userNames,newUserName)              
+                })
+            }
+            $scope.newName = ''
+            refreshData()
         }
         $scope.deleteUser = function(index){
             var user = userNames[index]
@@ -58,8 +90,9 @@
             })
             .success(function(data,status,headers,config){
                     console.log(data)
-                    removeData(index,userNames)
-            })  
+                    removeData(index,userNames)                  
+            }) 
+            refreshData() 
             
         }
         $scope.sendPost = function(){
@@ -88,10 +121,13 @@
                     console.log("user yok amk nereye yolluyon")
                 }
             }
+            $scope.username = ''
+            $scope.postBody = ''
+            refreshData()
         }
         
         var getValues = function(apiData){
-                     
+          userNames = [], userPosts = []           
           for(var i=0; i< apiData.length;i++){
               userNames[i] = apiData[i].username
               for(var j=0;j<apiData[i].posts.length;j++){                
@@ -108,12 +144,29 @@
             console.log(userNames)
             console.log(userPosts)
         }
+         
        var removeData = function(index,array){
            if(index > -1){
                 array.splice(index,1);
                 console.log('new array is : ' + array)
             }else{
-                console.log('index is < -1,so failed!')
+                console.log('index is < -1,so failed remove!')
             }
+       }
+       var updateData = function(index,array,value){
+           if(index > -1){
+               array.splice(index,0,value)              
+           }else{
+               console.log('index is <-1 , so failed update!')
+           }
+       }
+       var getUserId = function(name){
+           var userId =0;
+           for(var i = 0; i< apiData.length; i++){
+               if(apiData[i].username === name){
+                   userId = apiData[i]._id
+               }
+           }
+           return userId
        }   
     });   
